f56628c602afd800b19848044197b3c29766dd40
traderspostcom <russdjohnson@gmail.com>
2025-09-24 16:21:33 -0400

    Update health.js

--- src/health.js
+++ src/health.js
@@ -10,3 +10,17 @@ r.get('/', (req, res) => {
 });
 
 export default r;
+
+{
+  "name": "odds-backend-gpt",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "src/index.js",
+  "scripts": { "start": "node src/index.js" },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2",
+    "morgan": "^1.10.0"
+  }
+}
